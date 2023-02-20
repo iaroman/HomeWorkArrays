71342143521e0db27910a5ae55eb70fa14421570
Роман <roma_yakushin@mail.ru>
2023-02-20 22:42:22 +0300

    install

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+    public static void task1() {
+        System.out.println("[[[ Task 1 ]]]");
+
+    }
+}
